f307f4dfbeec40f8b8e650a507bc06cb7b827536
ibatullintima <ibatullintima@gmail.com>
2025-03-03 04:59:10 +0600

    add: adaptive with not sliders

--- script.js
+++ script.js
@@ -88,81 +88,81 @@ document.addEventListener("DOMContentLoaded", () => {
       cooperationF: "2025 ©",
       rightsF: "All rights reserved.",
     },
-  };
+  }
 
   function setLanguage(lang) {
-    document.querySelector("ul li:nth-child(1) a").textContent = translations[lang].cooperation;
-    document.querySelector("ul li:nth-child(2) a").textContent = translations[lang].brands;
-    document.querySelector("ul li:nth-child(3) a").textContent = translations[lang].about;
-    document.querySelector("ul li:nth-child(4) a").textContent = translations[lang].contacts;
-    document.querySelector(".head_phoneNumber p").textContent = translations[lang].callCenter;
-    document.querySelector(".head_contacts").textContent = translations[lang].contactUs;
+    document.querySelector("ul li:nth-child(1) a").textContent = translations[lang].cooperation
+    document.querySelector("ul li:nth-child(2) a").textContent = translations[lang].brands
+    document.querySelector("ul li:nth-child(3) a").textContent = translations[lang].about
+    document.querySelector("ul li:nth-child(4) a").textContent = translations[lang].contacts
+    document.querySelector(".head_phoneNumber p").textContent = translations[lang].callCenter
+    document.querySelector(".head_contacts").textContent = translations[lang].contactUs
 
     // Section Home
-    document.querySelector(".main_container_one h1").textContent = translations[lang].homeHeader;
-    document.querySelector(".main_container_one p").textContent = translations[lang].homeSubHeader;
+    document.querySelector(".main_container_one h1").textContent = translations[lang].homeHeader
+    document.querySelector(".main_container_one p").textContent = translations[lang].homeSubHeader
 
     // Marquee 
-    const marqueeSpans = document.querySelectorAll(".marquee span");
+    const marqueeSpans = document.querySelectorAll(".marquee span")
     marqueeSpans.forEach(span => {
-      span.textContent = translations[lang].marqueeT;
-    });
+      span.textContent = translations[lang].marqueeT
+    })
 
     // Section Guarantees
-    document.querySelectorAll(".main_bottom_content div h3")[0].textContent = translations[lang].guarantee1;
-    document.querySelectorAll(".main_bottom_content div h3")[1].textContent = translations[lang].guarantee2;
-    document.querySelectorAll(".main_bottom_content div h3")[2].textContent = translations[lang].guarantee3;
-    document.querySelectorAll(".main_bottom_content div h3")[3].textContent = translations[lang].guarantee4;
+    document.querySelectorAll(".main_bottom_content div h3")[0].textContent = translations[lang].guarantee1
+    document.querySelectorAll(".main_bottom_content div h3")[1].textContent = translations[lang].guarantee2
+    document.querySelectorAll(".main_bottom_content div h3")[2].textContent = translations[lang].guarantee3
+    document.querySelectorAll(".main_bottom_content div h3")[3].textContent = translations[lang].guarantee4
 
     // Section Brands
-    document.querySelector(".title_block h1").textContent = translations[lang].brandsHeader;
+    document.querySelector(".title_block h1").textContent = translations[lang].brandsHeader
     document.querySelector('.ourBrandsTitle').textContent = translations[lang].ourBrands
 
     // Section Products
-    document.querySelector(".slider_container h1").textContent = translations[lang].productsHeader;
+    document.querySelector(".slider_container h1").textContent = translations[lang].productsHeader
 
     // Section About Us
-    document.querySelector(".about_us_content1 h1").textContent = translations[lang].aboutUsHeader;
+    document.querySelector(".about_us_content1 h1").textContent = translations[lang].aboutUsHeader
 
-    document.querySelector(".ab_box_1 p").textContent = translations[lang].ourTxone;
-    document.querySelector(".ab_box_2 p").textContent = translations[lang].ourTtwo;
-    document.querySelector(".ab_box_3 p").textContent = translations[lang].ourTxThree;
-    document.querySelector(".ab_box_4 p").textContent = translations[lang].ourTxFor;
+    document.querySelector(".ab_box_1 p").textContent = translations[lang].ourTxone
+    document.querySelector(".ab_box_2 p").textContent = translations[lang].ourTtwo
+    document.querySelector(".ab_box_3 p").textContent = translations[lang].ourTxThree
+    document.querySelector(".ab_box_4 p").textContent = translations[lang].ourTxFor
 
 
     document.querySelector(".left_content2 h1").textContent = translations[lang].aboutUs2Title
-    document.querySelectorAll(".step_work_container .box_container_1 div h3")[0].textContent = translations[lang].step1;
-    document.querySelectorAll(".step_work_container .box_container_1 div p")[0].textContent = translations[lang].step1Desc;
-    document.querySelectorAll(".step_work_container .box_container_1 div h3")[1].textContent = translations[lang].step2;
-    document.querySelectorAll(".step_work_container .box_container_1 div p")[1].textContent = translations[lang].step2Desc;
-    document.querySelectorAll(".step_work_container .box_container_2 div h3")[0].textContent = translations[lang].step3;
-    document.querySelectorAll(".step_work_container .box_container_2 div p")[0].textContent = translations[lang].step3Desc;
-    document.querySelectorAll(".step_work_container .box_container_2 div h3")[1].textContent = translations[lang].step4;
-    document.querySelectorAll(".step_work_container .box_container_2 div p")[1].textContent = translations[lang].step4Desc;
+    document.querySelectorAll(".step_work_container .box_container_1 div h3")[0].textContent = translations[lang].step1
+    document.querySelectorAll(".step_work_container .box_container_1 div p")[0].textContent = translations[lang].step1Desc
+    document.querySelectorAll(".step_work_container .box_container_1 div h3")[1].textContent = translations[lang].step2
+    document.querySelectorAll(".step_work_container .box_container_1 div p")[1].textContent = translations[lang].step2Desc
+    document.querySelectorAll(".step_work_container .box_container_2 div h3")[0].textContent = translations[lang].step3
+    document.querySelectorAll(".step_work_container .box_container_2 div p")[0].textContent = translations[lang].step3Desc
+    document.querySelectorAll(".step_work_container .box_container_2 div h3")[1].textContent = translations[lang].step4
+    document.querySelectorAll(".step_work_container .box_container_2 div p")[1].textContent = translations[lang].step4Desc
 
     // FAQ Section
     document.querySelector('.text-center').textContent = translations[lang].reapeatFAQtitle
-    document.querySelectorAll(".faq-item .faq-question h1")[0].textContent = translations[lang].faq1;
-    document.querySelectorAll(".faq-item .faq-answer p")[0].textContent = translations[lang].faq1Ans;
-    document.querySelectorAll(".faq-item .faq-question h1")[1].textContent = translations[lang].faq2;
-    document.querySelectorAll(".faq-item .faq-answer p")[1].textContent = translations[lang].faq2Ans;
-    document.querySelectorAll(".faq-item .faq-question h1")[2].textContent = translations[lang].faq3;
-    document.querySelectorAll(".faq-item .faq-answer p")[2].textContent = translations[lang].faq3Ans;
+    document.querySelectorAll(".faq-item .faq-question h1")[0].textContent = translations[lang].faq1
+    document.querySelectorAll(".faq-item .faq-answer p")[0].textContent = translations[lang].faq1Ans
+    document.querySelectorAll(".faq-item .faq-question h1")[1].textContent = translations[lang].faq2
+    document.querySelectorAll(".faq-item .faq-answer p")[1].textContent = translations[lang].faq2Ans
+    document.querySelectorAll(".faq-item .faq-question h1")[2].textContent = translations[lang].faq3
+    document.querySelectorAll(".faq-item .faq-answer p")[2].textContent = translations[lang].faq3Ans
 
-    document.querySelector(".fAbout").textContent = translations[lang].aboutF;
-    document.querySelector(".fBrands").textContent = translations[lang].brandsF;
-    document.querySelector(".fProducts").textContent = translations[lang].productsF;
-    document.querySelector(".second_content_footer p:nth-child(1)").textContent = translations[lang].cooperationF;
-    document.querySelector(".second_content_footer p:nth-child(2)").textContent = translations[lang].rightsF;
+    document.querySelector(".fAbout").textContent = translations[lang].aboutF
+    document.querySelector(".fBrands").textContent = translations[lang].brandsF
+    document.querySelector(".fProducts").textContent = translations[lang].productsF
+    document.querySelector(".second_content_footer p:nth-child(1)").textContent = translations[lang].cooperationF
+    document.querySelector(".second_content_footer p:nth-child(2)").textContent = translations[lang].rightsF
 
-    document.getElementById("ru_translate").classList.remove("active");
-    document.getElementById("en_translate").classList.remove("active");
+    document.getElementById("ru_translate").classList.remove("active")
+    document.getElementById("en_translate").classList.remove("active")
 
-    document.getElementById(lang + "_translate").classList.add("active");
+    document.getElementById(lang + "_translate").classList.add("active")
   }
 
-  setLanguage("ru");
+  setLanguage("ru")
 
-  document.getElementById("ru_translate").addEventListener("click", () => setLanguage("ru"));
-  document.getElementById("en_translate").addEventListener("click", () => setLanguage("en"));
-});
+  document.getElementById("ru_translate").addEventListener("click", () => setLanguage("ru"))
+  document.getElementById("en_translate").addEventListener("click", () => setLanguage("en"))
+})
--- utils.js
+++ utils.js
@@ -1,13 +1,29 @@
 function toggleFAQ(element) {
-  let answer = element.querySelector('.faq-answer');
-  let icon = element.querySelector('.faq-question span');
+  let answer = element.querySelector('.faq-answer')
+  let icon = element.querySelector('.faq-question span')
   if (answer.classList.contains('show')) {
-    answer.classList.remove('show');
-    answer.style.display = 'none';
-    icon.textContent = '+';
+    answer.classList.remove('show')
+    answer.style.display = 'none'
+    icon.textContent = '+'
   } else {
-    answer.style.display = 'block';
-    setTimeout(() => answer.classList.add('show'), 10);
-    icon.textContent = '-';
+    answer.style.display = 'block'
+    setTimeout(() => answer.classList.add('show'), 10)
+    icon.textContent = '-'
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerMenu = document.getElementById('burger-menu')
+  const sideMenu = document.getElementById('side-menu').querySelector('.menu-container')
+
+  const closeMenu = document.getElementById('close-menu')
+
+
+  burgerMenu.addEventListener('click', () => {
+    sideMenu.classList.toggle('open')
+  })
+
+  closeMenu.addEventListener('click', () => {
+    sideMenu.classList.remove('open')
+  })
+})
